01e186bcd9899866bed51405daa8f9aa1c1c17ce
Moadbenammi <moadbenammi@gmail.com>
2022-01-03 21:40:34 +0100

    full project

---
+++ src/main/java/ma/pixel/employeemanager/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package ma.pixel.employeemanager.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
